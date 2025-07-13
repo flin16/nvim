39b426509a80fb9f12711ca005e7e5a73289f9d5
Fengyuan Lin <flin16@illinois.edu>
2025-07-13 17:35:23 -0500

    fix(init) add safe module call

--- init.lua
+++ init.lua
@@ -1,4 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.debugger").setup()
-pcall(require("config.local"))
+local function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+if file_exists(vim.fn.stdpath("config") .. "/lua/config/local.lua") then
+  require("config.local")
+end
